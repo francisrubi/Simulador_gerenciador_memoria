a93adf0d8a3232b671e1bbc9003fcb09ef5cbf56
Francis Rotilli <114816108+francisrubi@users.noreply.github.com>
2023-09-04 19:39:07 -0300

    Add files via upload

--- SistemaOperacional.py
+++ SistemaOperacional.py
@@ -2,8 +2,11 @@
 # necessário para permitir as entradas do usuário ao mesmo tempo que são executados os processos
 import threading
 import time
+import random
 
 from Processo import Processo
+from RAM import RAM
+from Disco import Disco
 
 class SistemaOperacional:
     def __init__(self, tam_ram, tam_disco, tam_pagina, quantum):
@@ -19,13 +22,8 @@ class SistemaOperacional:
             raise Exception('Tamanho da memória lógica não comporta o tamanho da página.')
         self.num_pag_logicas = int(tam_disco / tam_pagina)
 
-        self.tam_pagina = tam_pagina
-        self.tam_memoria_fisica = tam_ram
-        self.tam_memoria_virtual = tam_disco
-
         # inicializa uma lista com as páginas nas memórias
-        self.pag_fisicas = [self.Pagina(tam_pagina)] * self.num_pag_fisicas
-        self.pag_logicas = [self.Pagina(tam_pagina)] * self.num_pag_logicas
+        self.mem_fisica = RAM(num_pag_fisicas, num_pag_logicas, tam_pagina)
 
         # processo que está atualmente em execução
         self.processo_execucao = None
@@ -44,12 +42,6 @@ class SistemaOperacional:
 
     # classes internas de página para memória e de processo em fila
     #region Classes Internas
-    class Pagina():
-        def __init__(self, tamanho):
-            self.bytes = [0] * tamanho
-            self.ocupada = False
-            self.processo = None
-
     class ProcessoFila:
         def __init__(self, processo, tempo_chegada):
             self.processo = processo
@@ -57,6 +49,8 @@ class SistemaOperacional:
             self.TSF = None
     #endregion
     
+    #region Manipulação de processos
+
     def insere_processo_fila(self, processo):
         pf = self.ProcessoFila(processo, self.tempo_programa)
         self.fila_aptos.append(pf)
@@ -64,8 +58,15 @@ class SistemaOperacional:
     
     def novo_processo(self, nome, tamanho, tempo_processo):
         p = Processo(nome, self.tempo_programa, tempo_processo, tamanho)
+        self.mem_fisica.aloca_processo(p)
         self.insere_processo_fila(p)
-        print(1)
+
+    # def encerra_processo(self, pid):
+    #     self.fila_aptos = [p for p in self.fila_aptos if p.processo.PID != pid]
+    #     if (self.processo_execucao.PID == pid):
+    #         self.processo_execucao = None
+
+    #endregion
 
     # Inserir funções de print aqui
     #region Funções Print
@@ -90,7 +91,6 @@ class SistemaOperacional:
     #endregion
 
     def executa_processos(self):
-        print(1)
         while self.executando:
             em_execucao = self.processo_execucao
 
@@ -114,14 +114,15 @@ class SistemaOperacional:
                     tempo = (em_execucao.TP - em_execucao.TE)
                 
                 tempo_final = self.tempo_programa + tempo
-                while self.tempo_programa < tempo_final:
+                while self.tempo_programa < tempo_final and self.processo_execucao is not None:
                     time.sleep(1)
                     em_execucao.TE += 1
                     self.tempo_programa += 1
                 
-                if em_execucao.TE == em_execucao.TP:
-                    self.processo_execucao = None
-                    em_execucao.TT = self.tempo_programa
-                    em_execucao.executado = True
-                elif len(self.fila_aptos) > 0:
-                    self.insere_processo_fila(em_execucao)
+                if self.processo_execucao is not None:
+                    if em_execucao.TE == em_execucao.TP:
+                        self.processo_execucao = None
+                        em_execucao.TT = self.tempo_programa
+                        em_execucao.executado = True
+                    elif len(self.fila_aptos) > 0:
+                        self.insere_processo_fila(em_execucao)
